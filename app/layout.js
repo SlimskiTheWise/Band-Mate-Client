2a9bbb7b1966ea6baf87e0412e6326be66c06775
SlimskiTheWise <hun245600@gmail.com>
2023-07-04 10:01:00 +0900

    feature: add main page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { AuthContextProvider } from '@/components/shared/auth-context';
 import './globals.css';
 import { Navbar } from '@/components/nav/navbar';
+import { Footer } from '@/components/footer/footer';
 
 export const metadata = {
   title: 'Band-Mate',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <AuthContextProvider>
           <Navbar />
           {children}
+          <Footer/>
         </AuthContextProvider>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-
-
 export default function Home() {
-
   return (
-    <main className='space-x-2 p-4'>
-    </main>
+    <section class='mb-48'>
+      <div class='bg-neutral-50 py-24 px-6 text-center dark:bg-neutral-20'>
+        <h1 class='mt-2 mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl'>
+          Meet and Play <br />
+          <span class='text-primary'>and Sell</span>
+        </h1>
+        <a
+          class='mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 md:mr-2 md:mb-0'
+          data-te-ripple-init
+          data-te-ripple-color='light'
+          role='button'
+        >
+          Meet
+        </a>
+        <a
+           class='mb-2 inline-block rounded bg-primary px-12 pt-4 pb-3.5 text-sm font-medium leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 md:mr-2 md:mb-0'
+           data-te-ripple-init
+           data-te-ripple-color='light'
+           role='button'
+        >
+          Buy&Sell
+        </a>
+      </div>
+    </section>
   );
 }
