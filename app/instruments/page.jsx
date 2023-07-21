8aea7d275bac164d749e9c37e878e0ef2fcc3772
SlimskiTheWise <hun245600@gmail.com>
2023-07-21 18:30:34 +0900

    fix: changr placement of key

--- app/instruments/page.jsx
+++ app/instruments/page.jsx
@@ -118,11 +118,12 @@ const Page = () => {
 
         <div className='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
           {instruments?.items?.map((instrument) => (
-            <Link className='group' href={`/instruments/${instrument.id}`}>
-              <div
-                key={instrument.id}
-                className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7'
-              >
+            <Link
+              key={instrument.id}
+              className='group'
+              href={`/instruments/${instrument.id}`}
+            >
+              <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7'>
                 <img
                   src={`${process.env.NEXT_PUBLIC_S3_URL}${instrument?.picture}`}
                   alt='Tall slender porcelain bottle with natural clay textured body and cork stopper.'
