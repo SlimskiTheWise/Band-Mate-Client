144b000687a6aaa788bfde8660bdfca51c43d155
SlimskiTheWise <hun245600@gmail.com>
2023-07-24 10:22:54 +0900

    style: change style of the search bar

--- app/instruments/page.jsx
+++ app/instruments/page.jsx
@@ -16,7 +16,7 @@ function SearchBar({}) {
         <button
           id='dropdown-button'
           data-dropdown-toggle='dropdown'
-          className='flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-300 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600'
+          className='flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-300 dark:bg-neutral-600 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600'
           type='button'
         >
           All categories
@@ -82,13 +82,13 @@ function SearchBar({}) {
           <input
             type='search'
             id='search-dropdown'
-            className='block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'
+            className='block p-2.5 w-full z-20 text-sm text-gray-900 dark:bg-neutral-600 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:dark:bg-neutral-600 focus:dark:bg-neutral-600 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'
             placeholder='Search...'
             required
           />
           <button
             type='submit'
-            className='absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-gray-700 rounded-r-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-gray dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-gray-800'
+            className='absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-gray-700 rounded-r-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:dark:bg-neutral-600 dark:bg-gray-600 dark:hover:dark:bg-neutral-600 dark:focus:ring-gray-800'
           >
             <svg
               className='w-4 h-4'
@@ -223,9 +223,9 @@ const Page = () => {
 
   return (
     <div className='bg-white'>
+            <SearchBar />
       <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
         <h2 className='sr-only'>Products</h2>
-        <SearchBar />
         <div className='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
           {instruments?.items?.map((instrument) => (
             <Link
